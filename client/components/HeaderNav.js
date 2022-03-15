dcb79c72c8e01a9fbd7a91b468b72a91f3f3cea9
Ricky Chon <rickychon99@gmail.com>
2022-03-15 16:52:39 -0400

    Add HeaderNav component (#8)

---
+++ client/components/HeaderNav.js
@@ -0,0 +1,11 @@
+export const HeaderNav = () => (
+  <header className='header-nav'>
+    <HeaderNavTitle>Home</HeaderNavTitle>
+  </header>
+)
+
+const HeaderNavTitle = () => (
+  <span className="header-nav-title">
+    Home
+  </span>
+)
