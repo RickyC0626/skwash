c452267b929e4b2acaa77d5123a605a081fa12b7
Ricky Chon <rickychon99@gmail.com>
2022-03-22 19:31:34 -0400

    Section ProjectsPage tests by HTML elements

--- ui/__tests__/pages/projects.test.js
+++ ui/__tests__/pages/projects.test.js
@@ -4,31 +4,41 @@ import { render, shallow } from 'enzyme';
 import ProjectsPage from '../../pages/projects';
 
 describe('<ProjectsPage />', () => {
-  it('should be a div with id `#page-wrapper`', () => {
-    const wrapper = render(<ProjectsPage />);
-    expect(wrapper.is('div')).toBeTruthy();
-    expect(wrapper.attr('id')).toBe('page-wrapper');
+  describe('Container', () => {
+    it('should be a div with id `#page-wrapper`', () => {
+      const wrapper = render(<ProjectsPage />);
+      expect(wrapper.is('div')).toBeTruthy();
+      expect(wrapper.attr('id')).toBe('page-wrapper');
+    });
   });
 
-  it('should have a header', () => {
-    const wrapper = render(<ProjectsPage />);
-    expect(wrapper.find('header')).toHaveLength(1);
+  describe('Header', () => {
+    it('should exist', () => {
+      const wrapper = render(<ProjectsPage />);
+      expect(wrapper.find('header')).toHaveLength(1);
+    });
   });
 
-  it('should have main element', () => {
-    const wrapper = render(<ProjectsPage />);
-    expect(wrapper.find('main')).toHaveLength(1);
+  describe('Main section', () => {
+    it('should exist', () => {
+      const wrapper = render(<ProjectsPage />);
+      expect(wrapper.find('main')).toHaveLength(1);
+    });
   });
 
-  it('should have a footer', () => {
-    const wrapper = render(<ProjectsPage />);
-    expect(wrapper.find('footer')).toHaveLength(1);
-  })
+  describe('Footer', () => {
+    it('should exist', () => {
+      const wrapper = render(<ProjectsPage />);
+      expect(wrapper.find('footer')).toHaveLength(1);
+    });
+  });
 
-  it('should have horizontal rules between elements', () => {
-    const wrapper = render(<ProjectsPage />);
-    expect(wrapper.find('hr')).toHaveLength(2);
-  })
+  describe('Horizontal rule', () => {
+    it('should exist between elements', () => {
+      const wrapper = render(<ProjectsPage />);
+      expect(wrapper.find('hr')).toHaveLength(2);
+    });
+  });
 
   it('children should be in proper order', () => {
     const wrapper = render(<ProjectsPage />);
