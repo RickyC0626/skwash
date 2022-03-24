4e24cc2186967a3b98cadffac8bef1fa1aac5d37
Ricky Chon <rickychon99@gmail.com>
2022-03-23 22:01:33 -0400

    Add collapsible Sidebar logic

--- ui/components/Sidebar/Sidebar.js
+++ ui/components/Sidebar/Sidebar.js
@@ -1,109 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Sidebar.module.css';
-import { HomeIcon, ClockIcon, MenuIcon, MenuAlt2Icon } from '@heroicons/react/outline';
+import { HomeIcon, ClockIcon, MenuIcon, MenuAlt2Icon, MenuAlt3Icon } from '@heroicons/react/outline';
 
 import { DotIcon } from '../DotIcon';
 
-export const Sidebar = () => (
-  <aside className={styles.sidebar}>
-    <SbSection>
-      <div className='flex'>
-        <span className='p-2 grow'>LOGO</span>
-        <SbMenuButton>
-          <SbIcon Icon={MenuAlt2Icon} />
-        </SbMenuButton>
-      </div>
-    </SbSection>
-    <SbSection>
-      <SbList>
-        <SbListItem>
-          <SbLink href='#'>
-            <SbIcon Icon={HomeIcon} />
-            <SbLinkText>Home</SbLinkText>
-          </SbLink>
-        </SbListItem>
-        <SbListItem>
-          <SbLink href='#'>
-            <SbIcon Icon={MenuIcon} />
-            <SbLinkText>Menu</SbLinkText>
-          </SbLink>
-        </SbListItem>
-        <SbListItem>
-          <SbLink href='#'>
-            <SbIcon Icon={ClockIcon} />
-            <SbLinkText>Recent</SbLinkText>
-          </SbLink>
-        </SbListItem>
-      </SbList>
-    </SbSection>
-    <SbSection>
-      <SbSectionHeading>Teams</SbSectionHeading>
-      <SbList>
-        <SbListItem>
-          <SbLink href='#'>
-            <DotIcon className='fill-indigo-500' />
-            <SbLinkText>Engineering</SbLinkText>
-          </SbLink>
-        </SbListItem>
-        <SbListItem>
-          <SbLink href='#'>
-            <DotIcon className='fill-emerald-500' />
-            <SbLinkText>Human Resources</SbLinkText>
-          </SbLink>
-        </SbListItem>
-        <SbListItem>
-          <SbLink href='#'>
-            <DotIcon className='fill-amber-500' />
-            <SbLinkText>Customer Success</SbLinkText>
-          </SbLink>
-        </SbListItem>
-      </SbList>
-    </SbSection>
-  </aside>
-)
+export const Sidebar = () => {
+  const [isOpened, setOpen] = useState(true);
 
-const SbMenuButton = ({ children }) => (
-  <button className={styles['sidebar-menu-button']}>
-    {children}
-  </button>
-);
+  const toggleSidebar = () => setOpen(!isOpened);
 
-const SbSection = ({ children }) => (
-  <section className={styles['sidebar-section']}>
-    {children}
-  </section>
-);
+  const SbMenuButton = ({ children, onClick }) => (
+    <button onClick={onClick} className={styles['sidebar-menu-button']}>
+      {children}
+    </button>
+  );
 
-const SbSectionHeading = ({ children }) => (
-  <span className={styles['sidebar-section-heading']}>
-    {children}
-  </span>
-);
+  const SbSection = ({ children }) => (
+    <section className={styles['sidebar-section']}>
+      {children}
+    </section>
+  );
 
-const SbList = ({ children }) => (
-  <ul className={styles['sidebar-list']}>
-    {children}
-  </ul>
-);
+  const SbSectionHeading = ({ children }) => (
+    <span className={styles['sidebar-section-heading']}>
+      {children}
+    </span>
+  );
 
-const SbListItem = ({ children }) => (
-  <li className={styles['sidebar-list-item']}>
-    {children}
-  </li>
-);
+  const SbList = ({ children }) => (
+    <ul className={styles['sidebar-list']}>
+      {children}
+    </ul>
+  );
 
-const SbLink = ({ children, href }) => (
-  <a href={href} className={styles['sidebar-link']}>
-    {children}
-  </a>
-);
+  const SbListItem = ({ children }) => (
+    <li className={styles['sidebar-list-item']}>
+      {children}
+    </li>
+  );
 
-const SbIcon = ({ Icon }) => (
-  <Icon className={styles['sidebar-icon']} />
-);
+  const SbLink = ({ children, href }) => (
+    <a href={href} className={styles['sidebar-link']}>
+      {children}
+    </a>
+  );
 
-const SbLinkText = ({ children }) => (
-  <span className={styles['sidebar-link-text']}>
-    {children}
-  </span>
-);
+  const SbIcon = ({ Icon }) => (
+    <Icon className={styles['sidebar-icon']} />
+  );
+
+  const SbLinkText = ({ children }) => (
+    <span className={styles['sidebar-link-text']}>
+      {children}
+    </span>
+  );
+
+  return (
+    <aside className={`
+      ${styles['sidebar']}
+      ${isOpened ? styles['open'] : styles['closed']}
+    `}>
+      <SbSection>
+        <div className='flex'>
+          <span className={`
+            ${styles['sidebar-logo']}
+            ${isOpened ? styles['open'] : styles['closed']}
+          `}>LOGO</span>
+          <SbMenuButton onClick={toggleSidebar}>
+            <SbIcon Icon={isOpened ? MenuAlt3Icon : MenuAlt2Icon} />
+          </SbMenuButton>
+        </div>
+      </SbSection>
+      <SbSection>
+        <SbList>
+          <SbListItem>
+            <SbLink href='#'>
+              <SbIcon Icon={HomeIcon} />
+              <SbLinkText>Home</SbLinkText>
+            </SbLink>
+          </SbListItem>
+          <SbListItem>
+            <SbLink href='#'>
+              <SbIcon Icon={MenuIcon} />
+              <SbLinkText>My tasks</SbLinkText>
+            </SbLink>
+          </SbListItem>
+          <SbListItem>
+            <SbLink href='#'>
+              <SbIcon Icon={ClockIcon} />
+              <SbLinkText>Recent</SbLinkText>
+            </SbLink>
+          </SbListItem>
+        </SbList>
+      </SbSection>
+      <SbSection>
+        <SbSectionHeading>Teams</SbSectionHeading>
+        <SbList>
+          <SbListItem>
+            <SbLink href='#'>
+              <DotIcon className='fill-indigo-500' />
+              <SbLinkText>Engineering</SbLinkText>
+            </SbLink>
+          </SbListItem>
+          <SbListItem>
+            <SbLink href='#'>
+              <DotIcon className='fill-emerald-500' />
+              <SbLinkText>Human Resources</SbLinkText>
+            </SbLink>
+          </SbListItem>
+          <SbListItem>
+            <SbLink href='#'>
+              <DotIcon className='fill-amber-500' />
+              <SbLinkText>Customer Success</SbLinkText>
+            </SbLink>
+          </SbListItem>
+        </SbList>
+      </SbSection>
+    </aside>
+  );
+};
